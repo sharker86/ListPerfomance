251966d235064136cb561c84fd41c4a59694e472
sharker86 <21sqfe01>
2015-05-19 08:02:11 +0200

    a

--- app/src/main/java/com/example/joanisaac/listperfomance/MainActivity.java
+++ app/src/main/java/com/example/joanisaac/listperfomance/MainActivity.java
@@ -1,17 +1,52 @@
 package com.example.joanisaac.listperfomance;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private String[] values;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView listview = (ListView) findViewById(R.id.lista);
+        values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" , "Calculator"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                if (position >= 0 && position < values.length && values[position].equals("Calculator")) {
+                    //startCalculator();
+                }
+                view.setBackgroundColor(Color.argb(1, 20, 20, 20));
+            }
+
+        });
     }
 
     @Override
